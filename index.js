c85b561a68213a715d3a3f801b84055ca66913ec
Yalcin Imeryuz <yimeryuz@gmail.com>
2022-04-06 13:09:23 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+    window.open('mailto:yimeryuz@gmail.com?subject=Please enter everything you would like to purchase to build the PC.');
+});
